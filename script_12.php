2b8e448a08f86d5c3afee6b66978b79c39e3b64a
disha-vadaliya13 <143687159+disha-vadaliya13@users.noreply.github.com>
2023-09-02 08:30:02 +0530

    echo to display an unordered list (<ul>) with three list items (<li>) containing different colors.

---
+++ script_12.php
@@ -0,0 +1,7 @@
+<?php
+echo '<ul>';
+echo '<li>'."<font color=red>disha</font>".'</li>';
+echo '<li>'."<font color=blue>krishna</font>".'</li>';
+echo '<li>'."<font color=green>hemali</font>".'</li>';
+echo '</ul>';
+?>
